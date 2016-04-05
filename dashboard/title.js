a9f917b9169cb1124576d8dbde08b69e53908630
DbauchdSloth <dbauchdsloth@gmail.com>
2016-04-05 16:05:36 -0400

    fix element fetch

--- dashboard/title.js
+++ dashboard/title.js
@@ -1,7 +1,7 @@
 var mainTitle = nodecg.Replicant("mainTitle", {defaultValue: "DbauchdSloth"});
 var altTitle  = nodecg.Replicant("altTitle", {defaultValue: "Starting Soon"});
 
-document.querySelector('#title-submit').addEventListener('click', function(e) {
+document.getElementById('title-submit').addEventListener('click', function(e) {
   mainTitle = document.getElementById('main-title').value;
   atlTitle = document.getElementById('alt-title').value;
 })
