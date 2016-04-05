ce2dcb21c71488df52596a752e3fd2dff0e031f6
DbauchdSloth <dbauchdsloth@gmail.com>
2016-04-05 16:00:20 -0400

    Added replicant for title

--- dashboard/title.js
+++ dashboard/title.js
@@ -1,1 +1,8 @@
-console.dir(nodecg);
+
+var mainTitle = nodecg.Replicant("mainTitle", {defaultValue: "DbauchdSloth"}));
+var altTitle  = nodecg.Replicant("altTitle", {defaultValue: "Starting Soon"});
+
+document.querySelector('#title-submit').addEventListener('click', function(e) {
+  mainTitle = document.getElementById('main-title').value;
+  atlTitle = document.getElementById('alt-title').value;
+})
