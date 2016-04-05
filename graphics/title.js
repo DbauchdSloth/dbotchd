f1724dcb74e9dac8e2042e5505cc2dcec9f96077
DbauchdSloth <dbauchdsloth@gmail.com>
2016-04-05 16:21:32 -0400

    add text animation binding

--- graphics/title.js
+++ graphics/title.js
@@ -1,3 +1,7 @@
+$(function() {
+  $('.tlt').textillate({loop:true, minDisplayTime: 8000});
+})
+
 nodecg.Replicant('altTitle').on('change', function(oldValue, newValue) {
   document.getElementById("alt-title").textContent = newValue;
 });
